1d5f2e327f00cd38571450846cad3ef25cb0c972
ttpunch <vinod418@gmail.com>
2022-12-08 22:24:00 +0530

    asa

--- Backend/index.js
+++ Backend/index.js
@@ -17,16 +17,15 @@ const editFormRoute=require('./Router/editFormRoute')
 
 // Add Access Control Allow Origin headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-// app.use(
-//   cors({
-//   origin:"http://localhost:3000",
-//   methods:["GET","POST","PUT","DELETE"]
-// })
-// )
+app.use(
+  cors({
+    methods:["GET","POST","PUT","DELETE"]
+})
+)
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
